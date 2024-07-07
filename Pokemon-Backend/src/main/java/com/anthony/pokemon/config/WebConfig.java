29303c04d5f92ff71a19c56b045114cedc24f968
signOfI <alam14@seas.upenn.edu>
2024-07-06 18:06:48 -0700

    edit

--- Pokemon-Backend/src/main/java/com/anthony/pokemon/config/WebConfig.java
+++ Pokemon-Backend/src/main/java/com/anthony/pokemon/config/WebConfig.java
@@ -9,7 +9,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost")
+                .allowedOrigins(
+                        "http://localhost:8080",
+                        "http://54.215.203.52",
+                        "http://54.215.203.52:8080",
+                        "http://ec2-54-215-203-52.us-west-1.compute.amazonaws.com",
+                        "http://ec2-54-215-203-52.us-west-1.compute.amazonaws.com:8080"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
